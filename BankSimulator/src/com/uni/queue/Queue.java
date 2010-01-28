ecb4388401981e86efc03eb7745f2a521a86ac8c
audiofreq <audiofreq@9ae0b73a-06a1-11df-9763-65e337b7a424>
2010-01-28 15:54:43 +0000

    Queue class and package

---
+++ BankSimulator/src/com/uni/queue/Queue.java
@@ -0,0 +1,20 @@
+package com.uni.queue;
+
+import java.util.Stack;
+
+public class Queue<Customer> extends Stack<Customer>{
+
+	@Override
+	public synchronized Customer pop() {
+		// TODO Auto-generated method stub
+		return super.pop();
+	}
+
+	@Override
+	public Customer push(Customer item) {
+		// TODO Auto-generated method stub
+		return super.push(item);
+	}
+
+	
+}
