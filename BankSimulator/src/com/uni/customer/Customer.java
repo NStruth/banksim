9a007c71433b8155306db1436572166bdae2f68f
audiofreq <audiofreq@9ae0b73a-06a1-11df-9763-65e337b7a424>
2010-01-28 15:54:19 +0000

    Customer package and class

---
+++ BankSimulator/src/com/uni/customer/Customer.java
@@ -0,0 +1,12 @@
+package com.uni.customer;
+
+public class Customer {
+
+	String fName, lName;
+	String address;
+		
+	public Customer(String fName, String lName){
+		this.fName = fName;
+		this.lName = lName;
+	}
+}
