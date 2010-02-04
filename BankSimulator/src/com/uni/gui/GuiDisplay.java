205f05cda71bacdafe72870f40f2125798e5942f
audiofreq <audiofreq@9ae0b73a-06a1-11df-9763-65e337b7a424>
2010-02-04 15:56:27 +0000

    Gui package and basic class.

---
+++ BankSimulator/src/com/uni/gui/GuiDisplay.java
@@ -0,0 +1,13 @@
+package com.uni.gui;
+
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+
+public class GuiDisplay extends JFrame{
+
+	public GuiDisplay(){
+		super();
+		this.setBounds(50, 50, 200, 200);
+	}
+}
